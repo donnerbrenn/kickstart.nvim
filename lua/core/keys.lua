740e33c5b3fa3e2293b0f44f6613dd2c973945cc
thunder_burn <ml@viralwunder.de>
2024-06-08 20:46:25 +0200

    New Plugins, sorted keys

--- lua/core/keys.lua
+++ lua/core/keys.lua
@@ -4,47 +4,64 @@ local keymap = vim.keymap -- for conciseness()
 
 keymap.set('n', '<F9>', '<cmd>:w<CR><cmd>!make clean gen/shaders.h<CR>', { desc = 'Build shaders' })
 
-keymap.set('n', '<S-M-i>', 'gcc', { desc = '(un)comment line' })
-keymap.set('n', '<C-right>', '<cmd>bprevious<CR>', { desc = 'Jump to previous buffer' })
-keymap.set('n', '<C-left>', '<cmd>bnext<CR>', { desc = 'Jump to next buffer' })
+-- Alpha
+keymap.set('n', '<leader>a', ':Alpha<CR>', { desc = 'Toggle Alpha' })
 
+--Config
+keymap.set('n', '<leader>R', ':so %<CR>', { desc = 'Reload config' })
+
+--Sorting
+keymap.set('v', '<leader>s', ':sort<CR>', { desc = 'Sort lines' })
+keymap.set('v', '<leader>S', ':sort u<CR>', { desc = 'Sort lines and deduplicate' })
+
+-- Buffers
+keymap.set('n', '<C-b>', '<cmd>bd<CR>', { desc = 'Close current buffer' })
+
+-- Move around
+keymap.set('n', 'M-j', 'j^', { desc = 'Goto to the beginning of the next line' })
+keymap.set('n', 'M-up', 'k^', { desc = 'Goto to the beginning of the previos line' })
+keymap.set('n', '<C-left>', '<cmd>bnext<CR>', { desc = 'Jump to next buffer' })
+keymap.set('n', '<C-right>', '<cmd>bprevious<CR>', { desc = 'Jump to previous buffer' })
+keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move down and center' })
+
+keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Move up and center' })
+-- Move stuff around
 keymap.set('n', '<C-up>', '<cmd>m -2<CR>', { desc = 'Move line up' })
 keymap.set('n', '<C-down>', '<cmd>m +1<CR>', { desc = 'Move line down' })
-keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move down and center' })
-keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Move up and center' })
 
+-- Comments
+keymap.set('n', '<S-M-i>', 'gcc', { desc = '(un)comment line' })
+
+-- Bools
 keymap.set('n', '<M-t>', 'ciwtrue<ESC>', { desc = 'Remove word under the cursor and write "true"' })
 keymap.set('n', '<S-M-t>', 'ciwTrue<ESC>', { desc = 'Remove word under the cursor and write "True"' })
 keymap.set('n', '<M-f>', 'ciwfalse<ESC>', { desc = 'Remove word under the cursor and write "false"' })
 keymap.set('n', '<S-M-f>', 'ciwFalse<ESC>', { desc = 'Remove word under the cursor and write "False"' })
 
+-- Lazy
 keymap.set('n', '<leader>ls', '<cmd>Lazy<CR>', { desc = 'Go to Lazy' })
 keymap.set('n', '<leader>lu', '<cmd>Lazy update<CR>', { desc = 'Lazy update' })
+
+-- Mason
+keymap.set('n', '<leader>mu', '<cmd>MasonUpdate<CR>', { desc = 'Mason update' })
+
+-- Line numbers
 keymap.set('n', '<leader>n', '<cmd>set nu!<CR>', { desc = 'Switch line numbers' })
 keymap.set('n', '<leader>t', '<cmd>set rnu!<CR>', { desc = 'Switch relative line numbers' })
+
+-- Highlighting
 keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostics
 keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
---  See `:help wincmd` for a list of all window commands
-keymap.set('n', '<C-h>', '<:wincmd h<CR>', { desc = 'Move focus to the left window' })
-keymap.set('n', '<C-l>', '<:wincmd h>', { desc = 'Move focus to the right window' })
-keymap.set('n', '<C-j>', '<:wincmd h>', { desc = 'Move focus to the lower window' })
-keymap.set('n', '<C-k>', '<:wincmd h>', { desc = 'Move focus to the upper window' })
-
+-- Split windows
 keymap.set('n', '<leader>sv', '<C-w>v')
 keymap.set('n', '<leader>sh', '<C-w>s')
 keymap.set('n', '<leader>se', '<C-w>=')
 keymap.set('n', '<leader>sx', '<cmd>close<CR>')
-
--- TIP: Disable arrow keys in normal mode
-keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -76,7 +76,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
 		vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
 		vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
-		vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+		vim.keymap.set('n', '<leader><leader>', builtin.current_buffer_fuzzy_find, { desc = '[ ] Find in current buffer' })
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[ ] Find existing buffers' })
+		vim.keymap.set('n', '<leader>fc', builtin.colorscheme, { desc = 'Find colorscheme' })
 
 		-- Slightly advanced example of overriding default behavior and theme
 		vim.keymap.set('n', '<leader>/', function()
@@ -97,7 +99,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		end, { desc = '[S]earch [/] in Open Files' })
 
 		-- Shortcut for searching your Neovim configuration files
-		vim.keymap.set('n', '<leader>sn', function()
+		vim.keymap.set('n', '<leader>fn', function()
 			builtin.find_files { cwd = vim.fn.stdpath 'config' }
 		end, { desc = '[S]earch [N]eovim files' })
 	end,
