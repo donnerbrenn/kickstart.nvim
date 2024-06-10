551729ff9029b08c99983942d68f796be3b54d27
thunder_burn <ml@viralwunder.de>
2024-06-10 22:01:34 +0200

    Made lualine nicer

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,6 +3,16 @@ return {
 	event = 'VeryLazy',
 	opts = {
 		-- add any options here
+		messages = {
+			-- NOTE: If you enable messages, then the cmdline is enabled automatically.
+			-- This is a current Neovim limitation.
+			enabled = false, -- enables the Noice messages UI
+			view = false, -- default view for messages
+			view_error = false, -- view for errors
+			view_warn = false, -- view for warnings
+			view_history = false, -- view for :messages
+			view_search = false, -- view for search count messages. Set to `false` to disable
+		},
 	},
 	dependencies = {
 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
