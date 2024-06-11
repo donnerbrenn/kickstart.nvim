f3dc6f8d76c99d506343016901a466c67d7a9fdc
Manuel Lehmann <ml@viralwunder.de>
2024-06-11 12:53:59 +0200

    Fixed keys for flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -5,8 +5,8 @@ return {
 	opts = {},
   -- stylua: ignore
   keys = {
-    { "<leader>jj", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "<leader>js", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     -- { "<leader>jr", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     -- { "<leader>jR", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     -- { "<leader>jt", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
